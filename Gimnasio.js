db34013ede2650e0ce0bdbff689a0a266fb8c811
Oswaldo244 <oswaldogalindez8@gmail.com>
2021-03-11 20:12:07 -0500

    Se crea taller

---
+++ Gimnasio.js
@@ -0,0 +1,24 @@
+//Datos Entrada
+let peso=18.5;
+
+//Proceso 
+
+if(peso<=18.5){
+    console.log("Su Peso es Insuficiente")
+}else if(peso>18.5 && peso<=24.9){
+    console.log("Su Peso es Normal")
+}else if(peso>=25 && peso<=26.9){
+    console.log("Su Peso es Grado I")
+}else if(peso>=27 && peso<=29.9){
+    console.log("Su Peso es Grado II preobesidad")
+}else if(peso>=30 && peso<=34.9){
+    console.log("Su Peso es Obesidad de Tipo I")
+}else if(peso>=35 && peso<=39.9){
+    console.log("Su Peso es Obesidad de Tipo II")
+}else if(peso>=40 && peso<=49.9){
+    console.log("Su Peso es Obesidad de Tipo III Mórbida")
+}else if(peso>50){
+    console.log("Su Peso es Obesidad de Tipo IV Extrema")
+};
+
+//
