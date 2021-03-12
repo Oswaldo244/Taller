46c2c3ab7a7fc0e7bad69072302246e30f67143d
Oswaldo244 <oswaldogalindez8@gmail.com>
2021-03-12 11:07:07 -0500

    taller

--- Postobon.js
+++ Postobon.js
@@ -1,11 +1,11 @@
 //Postobon
 
-let horasTrabajadas = 37; 
+let horasTrabajadas = 40; 
 let salario = 20000 * horasTrabajadas;
-if(horasTrabajadas < 40) {
+if(horasTrabajadas <= 40) {
     console.log("Pagar a empleado ", salario);
 }else {
     let extraHoras = horasTrabajadas - 40;
-        salarioExtra += extraHoras * 250000;
+        let salarioExtra = extraHoras * 25000;
     console.log("Pagar a empleado", salarioExtra);
 }
